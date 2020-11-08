d9a49613e88ca4049ff21bf7a321d0c15d989414
Alison Williams <apw5011@gmail.com>
2020-11-08 23:08:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(sentence)
-  reversed_sentence = []
   sentence.split
-  sentence.each do |word|
-    reversed_sentence << word.reverse
-  end 
-  reversed_sentence
+  sentence.combine do |word|
+    word.reverse
+  end
 end
